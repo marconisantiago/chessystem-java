8c5a9a3b5ad0d716c7825aaaff69143575e97086
Marconi Santiago <marconi.santiago55@gmail.com>
2020-06-25 18:05:16 -0300

    Board and Pieces

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,16 +1,13 @@
 package application;
 
-import boadingGame.Position;
+import boadingGame.Board;
 
 public class Progam {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 				
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+	Board board = new Board(8,8);
 
 	}
 
---
+++ src/boadingGame/Board.java
@@ -0,0 +1,5 @@
+package boadingGame;
+
+public class Board {
+
+}
