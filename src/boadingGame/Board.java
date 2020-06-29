5bac4bea64e228b54a62de0fbf4045fb9f0dad08
Marconi Santiago <marconi.santiago55@gmail.com>
2020-06-29 11:31:29 -0300

    Placing pieces on the board

--- src/boadingGame/Board.java
+++ src/boadingGame/Board.java
@@ -4,8 +4,8 @@ public class Board {
 
 	private int rows;
 	private int columns;
-	private Piece[][]pieces;
-	
+	private Piece[][] pieces;
+
 	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
@@ -27,11 +27,19 @@ public class Board {
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
-	
-	public Piece piece (int row, int column){
+
+	public Piece piece(int row, int column) {
 		return pieces[row][column];
 	}
-	public Piece piece (Position position){
+
+	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+
+	public void placePiece(Piece piece, Position position) {
+
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+
 	}
 }
